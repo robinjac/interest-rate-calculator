4837a1901ec0fc5dc3e3b1a652ad85e5f553fc3f
robin jacobsson <robin.jacobsson@kebne.se>
2024-02-23 16:00:00 +0100

    WIP calculator

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,58 @@
-import { useState } from "react";
+import { type ChangeEventHandler, useState, createContext } from "react";
+
+type RateInput = { amount: number; rate: number };
+
+type State = {
+  income: {
+    fields: RateInput[];
+    average?: number;
+  };
+  expense: {
+    fields: RateInput[];
+    average?: number;
+  };
+};
+
+const state = createContext<State>({
+  income: { fields: [] },
+  expense: { fields: [] },
+});
+
+function NumberInput() {
+  const [number, setNumber] = useState<number | undefined>();
+
+  // Function to handle input change
+  const handleChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    const value = Number(event.target.value);
+
+    // Validate if the input is a number
+    if (!isNaN(value)) {
+      setNumber(value);
+    }
+  };
+
+  return (
+    <input
+      type="text"
+      value={number}
+      onChange={handleChange}
+    />
+  );
+}
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <span>Hello {count}</span>
-      <button onClick={() => setCount(count + 1)}>press me</button>
-    </>
+    <div>
+      <h1>Rate Gap Calculator</h1>
+      <div>
+        <h2>Rate expense</h2>
+        <NumberInput />
+
+        <h2>Rate income</h2>
+      </div>
+    </div>
   );
 }
 
