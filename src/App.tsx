dd7db179ddf30dc79a1fc8e33f3538c29f2c8258
robin jacobsson <robin.jacobsson@kebne.se>
2024-02-26 13:11:50 +0100

    Separate code

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { type ChangeEventHandler, useState } from "react";
+import { useState } from "react";
+import { Header, ListItem, type RateInput, type Input } from "./Components";
 
 type UUID = `${string}-${string}-${string}-${string}-${string}`;
-type RateInput = { amount: number; rate: number };
+
 type FieldItem = { rateInput: RateInput; id: UUID };
 type State = {
   income: {
@@ -14,82 +15,18 @@ type State = {
   };
 };
 type Category = keyof State;
-type Input = keyof RateInput;
 
-function NumberInput({
-  onChange,
-  unit,
-}: {
-  onChange: (value: number) => void;
-  unit: string;
-}) {
-  const [input, setInput] = useState<string>("");
 
-  // Function to handle input change
-  const handleChange: ChangeEventHandler<HTMLInputElement> = (event) => {
-    const value = event.target.value;
-    const number = parseFloat(value);
-
-    // Validate if the input is a number
-    if (!isNaN(number)) {
-      setInput(value);
-      onChange(number);
-    } else {
-      setInput("");
-    }
-  };
-
-  return (
-    <div className="inline-flex border-solid border border-slate-600 rounded">
-      <input
-        className="flex border-none rounded outline-none pl-1"
-        type="text"
-        value={input}
-        onChange={handleChange}
-      />
-      <span className="px-2 bg-slate-600 text-white">{unit}</span>
-    </div>
-  );
-}
-
-const Header = ({
-  text,
-  action,
-}: {
-  text: string;
-  action: { label: string; onClick: () => void };
-}) => (
-  <div className="flex border-b py-2 px-1 border-slate-600 justify-between">
-    <h2 className="text-xl">{text}</h2>
-    <button
-      onClick={action.onClick}
-      className="bg-slate-600 text-white px-2 py-1 text-sm border rounded"
-    >
-      {action.label}
-    </button>
-  </div>
-);
-
-const ListItem = ({
-  onRemove,
-  onChange,
-}: {
-  onRemove: () => void;
-  onChange: (input: Input, value: number) => void;
-}) => (
-  <div className="flex pb-5 px-1 justify-between">
-    <div className="space-x-2">
-      <NumberInput onChange={(value) => onChange("amount", value)} unit="kr" />
-      <NumberInput onChange={(value) => onChange("rate", value)} unit="%" />
-    </div>
-    <button
-      onClick={onRemove}
-      className="order-last bg-red-400 text-white px-3 text-lg rounded"
-    >
-      {"-"}
-    </button>
-  </div>
-);
+const initPatchFields = (state: State) => (
+  to: Category,
+  updater: (fields: FieldItem[]) => FieldItem[]
+) => ({
+  ...state,
+  [to]: {
+    ...state[to],
+    fields: updater(state[to].fields),
+  },
+});
 
 function App() {
   const [state, setState] = useState<State>({
@@ -97,16 +34,7 @@ function App() {
     expense: { fields: [] },
   });
 
-  const patchFields = (
-    to: Category,
-    updater: (fields: FieldItem[]) => FieldItem[]
-  ) => ({
-    ...state,
-    [to]: {
-      ...state[to],
-      fields: updater(state[to].fields),
-    },
-  });
+  const patchFields = initPatchFields(state);
 
   const add = (to: Category) => {
     const item: FieldItem = {
@@ -136,7 +64,7 @@ function App() {
       <h1 className="text-2xl">Rate Gap Calculator</h1>
       <div>
         <Header
-          text="Rate expense"
+          text={`Average Rate ${state.expense.average ?? 0}`}
           action={{ label: "Add +", onClick: () => add("expense") }}
         />
         <div className="pt-10">
---
+++ src/Components.tsx
@@ -0,0 +1,79 @@
+import { type ChangeEventHandler, useState } from "react";
+
+export type RateInput = { amount: number; rate: number };
+export type Input = keyof RateInput;
+
+export const NumberInput = ({
+  onChange,
+  unit,
+}: {
+  onChange: (value: number) => void;
+  unit: string;
+}) => {
+  const [input, setInput] = useState<string>("");
+
+  // Function to handle input change
+  const handleChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    const value = event.target.value;
+    const number = parseFloat(value);
+
+    // Validate if the input is a number
+    if (!isNaN(number)) {
+      setInput(value);
+      onChange(number);
+    } else {
+      setInput("");
+    }
+  };
+
+  return (
+    <div className="inline-flex border-solid border border-slate-600 rounded">
+      <input
+        className="flex border-none rounded outline-none pl-1"
+        type="text"
+        value={input}
+        onChange={handleChange}
+      />
+      <span className="px-2 bg-slate-600 text-white">{unit}</span>
+    </div>
+  );
+};
+
+export const Header = ({
+  text,
+  action,
+}: {
+  text: string;
+  action: { label: string; onClick: () => void };
+}) => (
+  <div className="flex border-b py-2 px-1 border-slate-600 justify-between">
+    <h2 className="text-xl">{text}</h2>
+    <button
+      onClick={action.onClick}
+      className="bg-slate-600 text-white px-2 py-1 text-sm border rounded"
+    >
+      {action.label}
+    </button>
+  </div>
+);
+
+export const ListItem = ({
+  onRemove,
+  onChange,
+}: {
+  onRemove: () => void;
+  onChange: (input: Input, value: number) => void;
+}) => (
+  <div className="flex pb-5 px-1 justify-between">
+    <div className="space-x-2">
+      <NumberInput onChange={(value) => onChange("amount", value)} unit="kr" />
+      <NumberInput onChange={(value) => onChange("rate", value)} unit="%" />
+    </div>
+    <button
+      onClick={onRemove}
+      className="order-last bg-red-400 text-white px-3 text-lg rounded"
+    >
+      {"-"}
+    </button>
+  </div>
+);
