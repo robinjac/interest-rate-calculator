012f9740e5807920c8f2a45fec43c3125591a621
robin jacobsson <robin.jacobsson@kebne.se>
2024-02-27 11:46:01 +0100

    Calculate the average rate

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import { Header, ListItem, type RateInput, type Input } from "./Components";
 
 type UUID = `${string}-${string}-${string}-${string}-${string}`;
@@ -16,17 +16,30 @@ type State = {
 };
 type Category = keyof State;
 
+const calculateAverage = (input: FieldItem[]) => {
+  const sum = input
+    .map(({ rateInput }) => rateInput.amount)
+    .reduce((sum_, val) => sum_ + val);
 
-const initPatchFields = (state: State) => (
-  to: Category,
-  updater: (fields: FieldItem[]) => FieldItem[]
-) => ({
-  ...state,
-  [to]: {
-    ...state[to],
-    fields: updater(state[to].fields),
-  },
-});
+  const weightedSum = input
+    .map(({ rateInput }) =>
+      rateInput.amount > 0 ? (rateInput.rate * rateInput.amount) / sum : 0
+    )
+    .reduce((avgRate, rate) => avgRate + rate, 0);
+
+  // Round to one decimal place
+  return Number(weightedSum.toFixed(1));
+};
+
+const initPatchFields =
+  (state: State) =>
+  (to: Category, updater: (fields: FieldItem[]) => FieldItem[]) => ({
+    ...state,
+    [to]: {
+      ...state[to],
+      fields: updater(state[to].fields),
+    },
+  });
 
 function App() {
   const [state, setState] = useState<State>({
@@ -34,7 +47,7 @@ function App() {
     expense: { fields: [] },
   });
 
-  const patchFields = initPatchFields(state);
+  const patchFields = useMemo(() => initPatchFields(state), [state]);
 
   const add = (to: Category) => {
     const item: FieldItem = {
@@ -64,7 +77,7 @@ function App() {
       <h1 className="text-2xl">Rate Gap Calculator</h1>
       <div>
         <Header
-          text={`Average Rate ${state.expense.average ?? 0}`}
+          text={`Average Rate ${calculateAverage(state.expense.fields) ?? 0}`}
           action={{ label: "Add +", onClick: () => add("expense") }}
         />
         <div className="pt-10">
